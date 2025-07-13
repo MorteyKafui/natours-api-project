7cabdcc2c91cf707f5b34b7f4f09c741d35e09d1
Kafui Mortey <morteykafui@gmail.com>
2025-07-13 12:39:32 +0000

    add db connection

--- server.mjs
+++ server.mjs
@@ -1,6 +1,11 @@
 import app from "./app.mjs";
+import mongoose from "mongoose";
 
 const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.DATABASE_URI, {}).then(() => {
+  console.log("Connected to DB...");
+});
 
 app.listen(PORT, () => {
   console.log("App running on port " + PORT);
